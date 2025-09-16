cd13bd2cd286c74727cdeb5e4a94504e20d267d3
dvaleriani <dvaleriani@robinson.it>
2025-09-16 11:57:21 +0200

    DLL
    Ristruttura del progetto.
    Gestione delle DLL

--- EVO_DBG/Public/js/components/Diagram/lib/types/ts-diagram/sources/helpers/placement.d.ts
+++
@@ -1,3 +0,0 @@
-import { ShapesCollection } from "../ShapesCollection";
-import { IDiagramConfig } from "../types";
-export declare function setHeaderColor(data: ShapesCollection, config?: IDiagramConfig): void;
